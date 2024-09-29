35ff5fb2f48d38c75f1e2b6e049c879490cac738
Herman <52775751+remove-checksum@users.noreply.github.com>
2024-09-29 11:52:08 +0300

    handle drop on enemy card

--- packages/sts-vue/src/components/EnemyCard.vue
+++ packages/sts-vue/src/components/EnemyCard.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import type { FunctionalComponent } from 'vue'
+import { onMounted, ref, type ComponentInstance, type FunctionalComponent } from 'vue'
 import Heart from '~icons/ph/heart-straight-fill'
 import Shield from '~icons/ph/shield-fill'
 import HalfDrop from '~icons/pixelarticons/drop-half'
 import StatusIcon from './StatusIcon.vue'
 import BaseCard from './BaseCard.vue'
+import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
 import type { Target, TargetStatus } from '@/model/character';
+import { assert } from '@/model/assert'
+import { getEnemyData } from '@/helpers/dragDataAssert'
 
 defineProps<{ selected: boolean, enemy: Target }>()
 const iconSizing = 'h-8 w-8 text-blue-400 md:h-12 md:w-12'
@@ -13,10 +16,26 @@ const iconStatusMap: Record<TargetStatus, { icon: FunctionalComponent, class: st
   bleed: { icon: HalfDrop, class: 'text-red-600' },
   block: { icon: Shield, class: 'text-stone-600' }
 }
+
+const enemyCardElement = ref<ComponentInstance<typeof BaseCard> | null>(null)
+
+onMounted(() => {
+  const element = enemyCardElement.value?.element
+  assert(element instanceof HTMLElement)
+
+  dropTargetForElements({
+    element,
+    getData: () => getEnemyData()
+  })
+
+})
 </script>
 
 <template>
-  <BaseCard :class="selected && 'bg-green-500'">
+  <BaseCard
+    ref="enemyCardElement"
+    :class="selected && 'bg-green-500'"
+  >
     <template #header>
       {{ enemy.name }}
     </template>
@@ -42,29 +61,4 @@ const iconStatusMap: Record<TargetStatus, { icon: FunctionalComponent, class: st
       </div>
     </template>
   </BaseCard>
-  <!-- <div
-    class="aspect-[0.72/1] xl:w-40 border-2 border-gray-200 p-4"
-    :class="[selected && 'bg-green-500']"
-  >
-    <h4 class="text-3xl">{{ enemy.name }}</h4>
-    <div class="flex items-start">
-      <StatusIcon :stat="enemy.health">
-        <Heart
-          class="text-pink-800"
-          :class="iconSizing"
-          :inline="true"
-        />
-      </StatusIcon>
-      <StatusIcon
-        v-for="[kind, amount] of enemy.statuses.entries()"
-        :key="kind"
-        :stat="amount"
-      >
-        <component
-          :is="iconStatusMap[kind].icon"
-          :class="[iconStatusMap[kind].class, iconSizing]"
-        />
-      </StatusIcon>
-    </div>
-  </div> -->
 </template>

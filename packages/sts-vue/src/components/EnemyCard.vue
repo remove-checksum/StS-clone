709cb7e01b7aa8917b9c1fdfc5568fd4d1f7a615
Herman <52775751+remove-checksum@users.noreply.github.com>
2024-09-29 15:03:00 +0300

    refactor: StatusIcon
    StatusIcon picks icon component and color based on DisplayTargetStat

--- packages/sts-vue/src/components/EnemyCard.vue
+++ packages/sts-vue/src/components/EnemyCard.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
-import { onMounted, ref, type ComponentInstance, type FunctionalComponent } from 'vue'
-import Heart from '~icons/ph/heart-straight-fill'
-import Shield from '~icons/ph/shield-fill'
-import HalfDrop from '~icons/pixelarticons/drop-half'
+import { onMounted, ref, type ComponentInstance } from 'vue'
 import StatusIcon from './StatusIcon.vue'
 import BaseCard from './BaseCard.vue'
 import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
-import type { Target, TargetStatus } from '@/model/character';
+import type { Target } from '@/model/character';
 import { assert } from '@/model/assert'
 import { getEnemyData } from '@/helpers/dragDataAssert'
 
 defineProps<{ selected: boolean, enemy: Target }>()
-const iconSizing = 'h-8 w-8 text-blue-400 md:h-12 md:w-12'
-const iconStatusMap: Record<TargetStatus, { icon: FunctionalComponent, class: string }> = {
-  bleed: { icon: HalfDrop, class: 'text-red-600' },
-  block: { icon: Shield, class: 'text-stone-600' }
-}
 
 const enemyCardElement = ref<ComponentInstance<typeof BaseCard> | null>(null)
 
@@ -41,22 +33,16 @@ onMounted(() => {
     </template>
     <template #content>
       <div class="flex">
-        <StatusIcon :stat="enemy.health">
-          <Heart
-            class="text-pink-800"
-            :class="iconSizing"
-            :inline="true"
-          />
-        </StatusIcon>
+        <StatusIcon
+          :stat="'health'"
+          :value="enemy.health"
+        ></StatusIcon>
         <StatusIcon
           v-for="[kind, amount] of enemy.statuses.entries()"
           :key="kind"
-          :stat="amount"
+          :stat="kind"
+          :value="amount"
         >
-          <component
-            :is="iconStatusMap[kind].icon"
-            :class="[iconStatusMap[kind].class, iconSizing]"
-          />
         </StatusIcon>
       </div>
     </template>
--- packages/sts-vue/src/components/StatusIcon.vue
+++ packages/sts-vue/src/components/StatusIcon.vue
@@ -1,33 +1,40 @@
 <script setup lang="ts">
-import { useMotion } from '@vueuse/motion'
-import { ref, watch } from 'vue'
+import { computed, ref, watch, type FunctionalComponent } from 'vue'
+import { type DisplayTargetStat } from '@/model/character';
+import Heart from '~icons/ph/heart-straight-fill'
+import Shield from '~icons/ph/shield-fill'
+import HalfDrop from '~icons/pixelarticons/drop-half'
+import Circle from '~icons/ph/circle-fill'
+import anime from 'animejs';
+import { assert } from '@/model/assert';
 
-const props = defineProps<{ stat: number | string }>()
+const props = defineProps<{ value: number | string, stat: DisplayTargetStat }>()
 const wrapperRef = ref<HTMLElement | null>(null)
 
+const iconStatusMap: Record<DisplayTargetStat, { component: FunctionalComponent, class: string }> = {
+	bleed: { component: HalfDrop, class: 'text-red-600' },
+	block: { component: Shield, class: 'text-stone-600' },
+	health: { component: Heart, class: 'text-pink-800' },
+	resource: { component: Circle, class: 'text-blue-400' }
+}
+
+const icon = computed(() => iconStatusMap[props.stat])
+
 // TODO: stagger animations in order
-const { apply } = useMotion(wrapperRef, {
-	initial: {
-		opacity: 1,
-		translateY: 0
-	},
-	jump: {
-		translateY: -5,
-		transition: {
-			duration: 100,
-			ease: 'backIn',
-			onComplete: () => apply('initial')
-		}
-	},
-	destroyed: {
-		translateX: 12,
-		backgroundColor: 'red'
+watch(() => props.value, (current, old) => {
+	if (current !== old) {
+		assert(wrapperRef.value instanceof HTMLDivElement)
+
+		anime({
+			targets: wrapperRef.value,
+			translateY: -15,
+			easing: 'easeInQuad',
+			duration: 200,
+			direction: 'alternate'
+		})
 	}
 })
 
-watch(() => props.stat, () => {
-	apply('jump')
-})
 
 </script>
 
@@ -36,7 +43,10 @@ watch(() => props.stat, () => {
 		ref="wrapperRef"
 		class="grid place-items-center *:col-start-1 *:row-start-1 *:col-end-1 *:row-end-1"
 	>
-		<slot></slot>
-		<span class="tabular-nums">{{ stat.toString() }}</span>
+		<component
+			:is="icon.component"
+			:class="[icon.class, 'h-8 w-8 text-blue-400 md:h-12 md:w-12', $attrs.class]"
+		></component>
+		<span class="tabular-nums">{{ value.toString() }}</span>
 	</div>
 </template>

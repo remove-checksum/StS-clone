af861d2a046385be5bc08344b2d6de9382f4520f
Herman <52775751+remove-checksum@users.noreply.github.com>
2024-09-29 14:09:07 +0300

    center player card

--- packages/sts-vue/src/components/PlayerCard.vue
+++ packages/sts-vue/src/components/PlayerCard.vue
@@ -1,29 +1,3 @@
-<template>
-	<div>
-		<h4 class="text-xl">{{ player.name }}</h4>
-		<div class="flex items-start">
-			<StatusIcon :stat="player.health">
-				<Heart
-					class="text-pink-800"
-					:class="iconSizing"
-					:inline="true"
-				/>
-			</StatusIcon>
-			<StatusIcon
-				v-for="[kind, amount] of player.statuses.entries()"
-				:key="kind"
-				:stat="amount"
-			>
-				<component
-					:is="iconStatusMap[kind][0]"
-					:class="[iconStatusMap[kind][1], iconSizing]"
-				/>
-			</StatusIcon>
-		</div>
-		<hr class="border-b-2 border-gray-700" />
-	</div>
-</template>
-
 <script setup lang="ts">
 import Heart from '~icons/ph/heart-straight-fill'
 import Shield from '~icons/ph/shield-fill'
@@ -31,6 +5,7 @@ import HalfDrop from '~icons/pixelarticons/drop-half'
 import StatusIcon from './StatusIcon.vue'
 import type { Player, TargetStatus } from '@/model/character'
 import { type FunctionalComponent } from 'vue'
+import BaseCard from './BaseCard.vue'
 
 defineProps<{
 	player: Player
@@ -42,3 +17,33 @@ const iconStatusMap: Record<TargetStatus, [FunctionalComponent, string]> = {
 	block: [Shield, 'text-stone-600']
 }
 </script>
+
+<template>
+	<BaseCard>
+		<template #header>
+			<h4 class="text-xl">{{ player.name }}</h4>
+		</template>
+		<template #content>
+
+			<div class="flex items-start">
+				<StatusIcon :stat="player.health">
+					<Heart
+						class="text-pink-800"
+						:class="iconSizing"
+						:inline="true"
+					/>
+				</StatusIcon>
+				<StatusIcon
+					v-for="[kind, amount] of player.statuses.entries()"
+					:key="kind"
+					:stat="amount"
+				>
+					<component
+						:is="iconStatusMap[kind][0]"
+						:class="[iconStatusMap[kind][1], iconSizing]"
+					/>
+				</StatusIcon>
+			</div>
+		</template>
+	</BaseCard>
+</template>

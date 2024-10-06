21834b2fcb28db8e67955bb8f70f21e8488f7846
Herman <52775751+remove-checksum@users.noreply.github.com>
2024-10-06 10:15:16 +0300

    feat: 404 route

--- packages/sts-vue/src/router/index.ts
+++ packages/sts-vue/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GameScreen from '../views/GameScreen.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,10 @@ const router = createRouter({
 		{
 			path: '/game',
 			component: GameScreen
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			component: NotFound
 		}
 	]
 })
---
+++ packages/sts-vue/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+import { RouterLink, useRoute } from 'vue-router';
+
+const route = useRoute()
+
+</script>
+
+<template>
+  <div class="px-4">
+    <h1>Page {{ route.fullPath }} not found!</h1>
+    <RouterLink
+      to="/game"
+    >
+      <BaseButton>
+        Back to game
+      </BaseButton>
+    </RouterLink>
+  </div>
+</template>
